4ad0b20c8abae30993fcd36addb81d7a31f6f8f8
Leif Gensert <leif@leif.io>
2017-06-09 19:06:39 +0200

    make library available in Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Backports.Mixfile do
   def project do
     [app: :backports,
      version: "0.1.0",
-     elixir: "~> 1.1",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      elixirc_paths: elixirc_paths(Mix.env),
