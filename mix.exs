554dfa5f6dd8e92d7ae240ade3e41b92f4acd4f1
Leif Gensert <leif@leif.io>
2017-06-09 18:59:14 +0200

    make ready for CI

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Backports.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     elixirc_paths: elixirc_paths(Mix.env),
      deps: deps()]
   end
 
@@ -17,6 +18,9 @@ defmodule Backports.Mixfile do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   # Dependencies can be Hex packages:
   #
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,1 @@
-alias Wrapper
 ExUnit.start()
