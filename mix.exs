5cf86a3513cd5ce73a88139d19200cc5d65a779a
Leif Gensert <leif@leif.io>
2017-06-11 21:53:11 +0200

    make package releasable

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule Backports.Mixfile do
   def project do
     [app: :backports,
      version: "0.1.0",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      elixirc_paths: elixirc_paths(Mix.env),
      deps: deps()]
   end
@@ -32,6 +34,22 @@ defmodule Backports.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    """
+    Precompiler to make new code work with old Elixir versions
+    """
+  end
+
+  defp package do
+    [
+      name: :backports,
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Leif Gensert"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/leifg/backports"}
+    ]
   end
 end
