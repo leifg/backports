a38a788018374582a88d74128e1ddf4952e00aae
Leif Gensert <leif@leif.io>
2017-06-13 09:15:32 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Backports.Mixfile do
 
   def project do
     [app: :backports,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
